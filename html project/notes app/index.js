74558f754868991db3dfa69352c0d35a6ceed9f5
sambhav6496 <jainsam620@gmail.com>
2021-04-02 17:13:17 +0530

    intersection

---
+++ html project/notes app/index.js
@@ -0,0 +1,43 @@
+const addBtn = document.getElementById('add')
+
+
+addBtn.addEventListener('click' , ()=>{
+   addNewNote()
+})
+
+function addNewNote(){
+    const note = document.createElement('div')
+    note.classList.add('note')
+    note.innerHTML = `
+    <div class="notes">
+    <div class="tools">
+      <button class="edit"><i class="fas fa-edit"></i></button>
+      <button class="delete"><i class="fas fa-trash"></i></button>
+    </div>
+    <div class="main hidden"></div>
+    <textarea ></textarea>
+  </div>`;
+
+const editBtn = note.querySelector(".edit")
+const deleteBtn = note.querySelector(".delete")
+const main = note.querySelector('.main')
+const textArea = note.querySelector('textArea')
+
+editBtn.addEventListener("click" , function(){
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+})
+
+deleteBtn.addEventListener('click', function(){
+    note.remove()
+})
+
+
+textArea.addEventListener("input" , (e)=> {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value)
+})
+
+    document.body.appendChild(note)
+}
--- linked list/intersection.js
+++ linked list/intersection.js
@@ -11,17 +11,47 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function (headA, headB) {
-  let currA = headA;
-  let currB = headB;
-  while (currA !== null) {
-    while (currB !== null) {
-      if ((currA.val = currB.vale)) {
-        return currA.val;
-      }
-      currB = currB.next;
+var getIntersectionNode = function(headA, headB) {
+    if(headA === null || headB === null){
+        return null
     }
-    currA = currA.next;
-  }
-  return null;
-};
+    let lengthA = 0
+    let lengthB = 0
+    let length  = 0
+    let tempLength = 0
+    let currA = headA
+    let currB = headB
+    while(currA){
+        lengthA++
+        currA = currA.next
+    }
+    while(currB){
+        lengthB++
+        currB = currB.next 
+    }
+ 
+    
+       if(lengthA > lengthB ){
+    tempLength = lengthA - lengthB
+      for(let i=0; i<tempLength; i++){
+          currA = currA.next
+          length = lengthB
+      }
+    }
+    
+     if(lengthB > lengthA ){
+    tempLength = lengthB - lengthA
+      for(let i=0; i<tempLength; i++){
+          currB = currB.next
+          length = lengthA
+      }
+    }
+    while(length >= 0){
+        if(currA === currB){
+            return currA
+        }
+        currA = currA.next
+        currB = currB.next
+        length--
+    }
+    return null
